cfe6c175136488c2b917abddd61eafc4e8de19d5
Wooseung Cho <main.wooseung@gmail.com>
2023-08-03 21:42:24 +0900

    컴포넌트에서 특정 sets 을 추가 할 경우 rig_sets 에 자동으로 추가하도록 수정.

--- scripts/domino/assembler/__init__.py
+++ scripts/domino/assembler/__init__.py
@@ -1483,6 +1483,8 @@ def create_rig(guide=None, rig=None, data=None, context=None):
         mc.connectAttr(geometry_sets + ".message", context["asset"][1] + ".geo_sets")
         mc.connectAttr(controller_sets + ".message", context["asset"][1] + ".ctl_sets")
         mc.connectAttr(skeleton_sets + ".message", context["asset"][1] + ".jnt_sets")
+        if context["specific_sets"]:
+            mc.sets(context["specific_sets"], edit=True, addElement=rig_sets)
 
         # callback
         if context["callbacks"]:
