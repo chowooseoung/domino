29c67e003a87662d5724596042c9c20e8a1137aa
Wooseung Cho <main.wooseung@gmail.com>
2023-12-31 10:36:02 +0900

    polygon update

--- scripts/domino/lib/polygon.py
+++ scripts/domino/lib/polygon.py
@@ -1,5 +1,12 @@
 # maya
 from maya import cmds as mc
+from maya.api import OpenMaya as om2
+
+# domino
+from domino.lib import vector
+
+# built-ins
+import math
 
 
 def get_component_index(component):
@@ -38,3 +45,74 @@ def convert_component(component, vertex=False, edge=False, face=False, uv=False,
     elif uv:
         result_component = "map"
     return [mesh + "." + result_component + "[" + index + "]" for index in sorted_index_range]
+
+
+def loop_to_2way(loop, start_vertex, end_vertex):
+    """
+
+    Args:
+        loop : vertex loop. ["plane1.vtx[1]", ...]
+        start_vertex : vtx. "plane1.vtx[1]"
+        end_vertex : vtx. "plane1.vtx[10]"
+    """
+    if start_vertex not in loop:
+        return [], []
+    if end_vertex not in loop:
+        return [], []
+
+    way1 = [start_vertex]
+    way2 = [start_vertex]
+    loop.remove(start_vertex)
+
+    def get_ways():
+        if loop and (end_vertex not in way1 or end_vertex not in way2):
+            way_vertexes = []
+            if end_vertex not in way1:
+                way_vertexes.append([way1, way1[-1]])
+            if end_vertex not in way2:
+                way_vertexes.append([way2, way2[-1]])
+            for way, vtx in way_vertexes:
+                edges = mc.polyListComponentConversion(vtx, fromVertex=True, toEdge=True)
+                for v in convert_component(edges, vertex=True):
+                    if v in loop:
+                        way.append(v)
+                        if v != end_vertex:
+                            loop.remove(v)
+                        break
+            get_ways()
+
+    get_ways()
+    return way1, way2
+
+
+def point_to_mesh(parent, name, points, up=(0, 0, 1)):
+    total_length = 0
+    for i in range(len(points)):
+        if i == 0:
+            continue
+        total_length += vector.get_distance(points[i], points[i - 1])
+    if total_length > 2:
+        up = om2.MVector(up) / math.log2(total_length)
+    else:
+        up = om2.MVector(up) * 0.05
+
+    crv1 = mc.curve(point=points, name="TEmp1", degree=1)
+    crv2 = mc.curve(point=[om2.MVector(p) + up for p in points], name="TEmp2", degree=1)
+    crv3 = mc.curve(point=[om2.MVector(p) - up for p in points], name="TEmp3", degree=1)
+
+    mesh = mc.loft([crv2, crv1, crv3],
+                   name=name,
+                   constructionHistory=0,
+                   uniform=1,
+                   close=0,
+                   autoReverse=1,
+                   degree=1,
+                   sectionSpans=1,
+                   range=0,
+                   rebuild=0,
+                   polygon=1,
+                   reverseSurfaceNormals=True)[0]
+    if parent:
+        mesh = mc.parent(mesh, parent)[0]
+    mc.delete([crv1, crv2 ,crv3])
+    return mesh
